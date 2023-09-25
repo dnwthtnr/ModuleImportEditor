d737bbc9a5e41cf42521042b13959f2048bda30c
dnwthtnr <dunworthtanner@gmail.com>
2023-09-25 19:01:10 -0400

    Initial commit

---
+++ source/ModuleImportEditor/__init__.py
@@ -0,0 +1,195 @@
+import os, sys, re
+
+from source import file_management
+
+
+configPath = "config.json"
+
+def readConfig(path=configPath):
+    """
+    Reads the given config path
+    Parameters
+    ----------
+    path
+
+    Returns
+    -------
+    dict
+        Data from config
+
+    """
+    if not os.path.exists(path):
+        raise FileNotFoundError(f'File: {path} does not exist.')
+
+    data = file_management.read_json(path)
+    return data
+
+def writeConfig(data, outputPath=configPath):
+    """
+    Writes teh given config data
+
+    Parameters
+    ----------
+    data
+    outputPath
+
+    """
+    if not isinstance(data, dict):
+        raise TypeError(f'Given data must be dictionary')
+    file_management.write_json(path=outputPath, data=data)
+
+
+# region Perform Replacement Actions
+
+def parseAndReplace(targetString, regexPattern, replacementText):
+    """
+    Replaces text matching the given regex expression with the replacementText
+
+    Parameters
+    ----------
+    targetString : str
+        Text to run replacement on
+    regexPattern : str
+        Regex expression to match text in file to
+    replacementText : str
+        Text to replace matches with
+
+    Returns
+    -------
+    str
+        Resulting text after replacements
+
+    """
+    resultString = re.sub(
+        pattern=regexPattern,
+        repl=replacementText,
+        string=targetString,
+        count=0,
+        flags=re.MULTILINE
+    )
+    return resultString
+
+
+def isValidSubstitionList(substitutionList):
+    """
+    Checks if teh given list in a valid substitution list
+
+    Parameters
+    ----------
+    substitutionList : list
+        The list to check validity of
+
+    Returns
+    -------
+    bool
+        Whether or not the given list is a valid substitution list
+
+    """
+    if not isinstance(substitutionList, list):
+        return False
+    if len(substitutionList) != 2:
+        return False
+    if False in [isinstance(_item, str) for _item in substitutionList]:
+        return False
+
+    return True
+
+
+def iteritiveParseAndReplace(targetString, substitutions):
+    """
+    Iterates over given substitution lists and runs the substituions on the given target string
+
+    Parameters
+    ----------
+    targetString : str
+        String to run substitutions on
+    substitutions : list[list[str, str]]
+        Substitutuions to run on target string
+
+    Returns
+    -------
+    str
+        The resulting string
+
+    """
+    resultString = targetString
+    for substitutionList in substitutions:
+        if not isValidSubstitionList(substitutionList):
+            TypeError(f'Substitution list: {substitutionList} is not valid. Skipping')
+            continue
+        pattern = substitutionList[0]
+        replacementText = substitutionList[1]
+
+        resultString = parseAndReplace(targetString=targetString, regexPattern=pattern, replacementText=replacementText)
+
+    return resultString
+
+
+def parseAndReplaceModule(moduleDirectory, outputDirectory, substitutions, fileExtension=[".py"]):
+    """
+    Given a directory will search through and run the given substitutions on the files with the given extensions.
+
+    Parameters
+    ----------
+    moduleDirectory : str
+        The directory to run the substitutions on
+    outputDirectory : str
+        Directory to write the new files to
+    substitutions : list[list[str, str]]
+        A list containing lists defining a regex pattern and the text to replace that pattern with
+    fileExtension : list[str]
+        The file extensions to run substitutions on
+
+    """
+
+
+    # region Run Replacements on Files
+    targetFiles = [
+        _filename for _filename in os.listdir(moduleDirectory) if file_management.getFilepathSuffix(_filename) in fileExtension
+    ]
+    for filename in targetFiles:
+        _filepath = os.path.join(moduleDirectory, filename)
+        _fileData = file_management.readFile(_filepath)
+
+        if not isinstance(_fileData, str):
+            continue
+
+        _result = iteritiveParseAndReplace(targetString=_fileData, substitutions=substitutions)
+        _outputFilepath = os.path.join(outputDirectory, filename)
+        file_management.writeFile(_outputFilepath, _result)
+    # endregion
+
+    # region Run Recursively On Child Directories
+    targetDirs = [_file for _file in os.listdir(moduleDirectory) if os.path.isdir(os.path.join(moduleDirectory, _file))]
+    for dir in targetDirs:
+        _fulldir = os.path.join(moduleDirectory, dir)
+        _newOutputDir = os.path.join(outputDirectory, dir)
+        if not os.path.exists(_newOutputDir):
+            os.mkdir(_newOutputDir)
+        parseAndReplaceModule(_fulldir, _newOutputDir, substitutions, fileExtension)
+    # endregion
+# endregion
+
+
+if __name__ == "__main__":
+    test = ("from pyqt_interface_elements import constants, buttons, icons\n"
+	"from . import constants, buttons, icons\n"
+	"from pyqt_interface_elements import constants, buttons, icons\n"
+	"from pyqt_interface_elements import constants, buttons, icons\n"
+	"from pyqt_interface_elements import constants, buttons, icons\n"
+	"from pyqt_interface_elements import constants, buttons, icons\n"
+	"from pyqt_interface_elements import constants, buttons, icons\n"
+	"from pyqt_interface_elements import constants, buttons, icons\n"
+	"from pyqt_interface_elements import constants, buttons, icons\n"
+	"from pyqt_interface_elements import constants, buttons, icons\n"
+	"from pyqt_interface_elements import constants, buttons, icons\n")
+
+    # print(parseAndReplace(test, r"from pyqt_interface_elements ", "from . "))
+
+    # print(os.listdir("Q:\__packages\_GitHub\ModuleImportEditor\source"))
+
+    # print(isValidSubstitionList(["(from pyqt_interface_elements )", "from . "]))
+
+    testpath = r"Q:\__packages\_GitHub\testfilepath"
+    output = r"Q:\__packages\_GitHub\testpath2"
+    parseAndReplaceModule(moduleDirectory=testpath, outputDirectory=output, substitutions=[["(from pyqt_interface_elements )", "from . "]])
